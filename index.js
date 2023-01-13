712b583e1084730fe22d8997932a1a8d41ef4e15
Varlince Meran <varlincemeran@gmail.com>
2023-01-13 10:32:08 -0500

    sales receipt

---
+++ index.js
@@ -0,0 +1,25 @@
+// create a sales receipt summary given subtotal and taxRate
+
+// const salesReceiptItems = {
+//     items : []
+// }
+
+function calculateSalesTax(subtotal, taxRate){
+    return ((subtotal * taxRate))
+}
+
+//.toFixed(2)
+
+//CONST = dont let it change while it is running
+//screaming snake case
+const TAX_RATE = .07    // For Palm Beach County, FL, US
+const customerSubtotal = 17.25
+
+//Calculate all the stuffs:
+const customerTaxTotal = calculateSalesTax(customerSubtotal, TAX_RATE)
+const customerTotal = customerSubtotal + customerTaxTotal
+
+console.log(`Subtotal       $${customerSubtotal}`)
+console.log(`Tax            $ ${customerTaxTotal.toFixed(2)}`)
+console.log(`-----------------------`)
+console.log(`Total          $${customerTotal.toFixed(2)}`)
